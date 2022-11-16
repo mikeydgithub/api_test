7bbaeceb10a2a77f19e34ae31f271be48f0903c7
mikeydgithub <ampgzo2@gmail.com>
2022-11-16 10:53:48 -0500

    set useeffect to do a fetch api call and then create json promise and extract json for a response

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+// css styling import
+import './App.css';
+// import usestate, useEffect from react library
+import { useState, useEffect } from 'react';
+
+// parent component
+// renderes everything from index.tsx
 function App() {
+  // all states will return an array
+  const [state, setState] = useState([])
+
+
+
+  // useEffect in a callback function
+  useEffect (() => {
+    // use fetch to get a promise
+    fetch('https://dev.everdays.com/v2/public/providers?state=mi')
+    // .then to take the promise and get a response
+    // extract json from the response
+    .then(response => response.json())
+    // resolve the response
+    // display and list the datat
+    .then(res => console.log(res))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// getting root from public/index.html
+// all of the logic goes inside the root component
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// load components
 root.render(
   <React.StrictMode>
     <App />
