9439aba7a3120b80b49f46520409437cde0e2228
mikeydgithub <ampgzo2@gmail.com>
2022-11-17 10:08:33 -0500

    axios commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 
-// css styling import
-import './App.css';
+import axios from 'axios';
 
 function App() {
-  
-  return (
-    
-    <div className="App">
-    </div>
-   
-   );
+  const getHomes = () => {
+    axios.get('https://dev.everdays.com/v2/public/providers?state=mi')
+    .then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
   }
   
+  return (
+    <div className="App">
+      <button onClick={getHomes}>Get Homes</button>
+    </div>
+   );
+  }
   export default App;
 
